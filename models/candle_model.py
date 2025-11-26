92b891eabad8595d365ef0d4a89379c1ee61613e
rmfabiousa-pixel <rmfabiousa@gmail.com>
2025-11-25 21:18:23 -0500

    Create candle_model.py

---
+++ models/candle_model.py
@@ -0,0 +1,16 @@
+class Candle:
+    def __init__(self, open, high, low, close, volume):
+        self.open = open
+        self.high = high
+        self.low = low
+        self.close = close
+        self.volume = volume
+
+    def to_dict(self):
+        return {
+            "open": self.open,
+            "high": self.high,
+            "low": self.low,
+            "close": self.close,
+            "volume": self.volume
+        }
